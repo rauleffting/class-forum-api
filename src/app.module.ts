72cab8d968df5895c14b062360edb7c82ac88be9
Raul Effting <rauleffting94@gmail.com>
2023-10-25 17:22:14 -0300

    feat(env): add envSchema and configure global variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
   // module brings together everything that is inside the app
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService], // everything which is not a controller is a provider on nest
 })
