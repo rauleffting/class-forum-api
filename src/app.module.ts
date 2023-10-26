ebdad6d04c3e024c2ebc786b3688dbeb32fa8063
Raul Effting <rauleffting94@gmail.com>
2023-10-26 10:53:04 -0300

    feat(modules): add new controllers to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   // module brings together everything that is inside the app
@@ -15,7 +17,12 @@ import { AuthenticateController } from './controllers/authenticate.controller'
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController,
+  ],
   providers: [PrismaService], // everything which is not a controller is a provider on nest
 })
 export class AppModule {}
