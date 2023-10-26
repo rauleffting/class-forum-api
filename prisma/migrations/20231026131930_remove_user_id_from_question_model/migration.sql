cb9b14834f881131fc81577d2a4b19e6ad624de2
Raul Effting <rauleffting94@gmail.com>
2023-10-26 10:20:10 -0300

    fix(orm): remove userId from Question model

---
+++ prisma/migrations/20231026131930_remove_user_id_from_question_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `questions` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "questions" DROP COLUMN "userId";
