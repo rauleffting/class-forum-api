0389add9cc1f299fdcc193e25622e5d2f7027996
Raul Effting <rauleffting94@gmail.com>
2023-10-27 15:57:36 -0300

    feat(entities): add email and password to StudentProps

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -1,10 +1,24 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface StudentProps {
+export interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(
     props: StudentProps, // With the Optional hack, we don't need to pass the createdAt propertie to create a question
     id?: UniqueEntityID,
