26553c470c25c8d743f33ba33feb5027d96aa8dc
Raul Effting <rauleffting94@gmail.com>
2023-10-27 21:20:55 -0300

    feat(auth): enable authentication globally

--- src/infra/auth/jwt-auth.guard.ts
+++ src/infra/auth/jwt-auth.guard.ts
@@ -1,3 +1,25 @@
+import { ExecutionContext, Injectable } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
+import { IS_PUBLIC_KEY } from './public'
 
-export class JwtAuthGuard extends AuthGuard('jwt') {} // this means that I want to use the passport-jwt strategy
+@Injectable() // this means that I want to use the passport-jwt strategy
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super()
+  }
+
+  // Enable authentication globally
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (isPublic) {
+      return true
+    }
+
+    return super.canActivate(context)
+  }
+}
