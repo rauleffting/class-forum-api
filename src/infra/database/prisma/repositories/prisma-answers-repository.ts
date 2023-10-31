f7a50a63ae40b55afc0768269886eb72d0d78661
Raul Effting <rauleffting94@gmail.com>
2023-10-30 22:34:23 -0300

    feat(orm-repositories): add PrismaAnswersRepository

---
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -0,0 +1,70 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { Injectable } from '@nestjs/common'
+import { PrismaAnswerMapper } from '../mappers/prisma-answer-mapper'
+import { PrismaService } from '../prisma.service'
+
+@Injectable()
+export class PrismaAnswersRepository implements AnswersRepository {
+  constructor(private prisma: PrismaService) {} // needed to inject prisma inside the repository
+
+  async findById(id: string): Promise<Answer | null> {
+    const answer = await this.prisma.answer.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    if (!answer) {
+      return null
+    }
+
+    return PrismaAnswerMapper.toDomain(answer)
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = await this.prisma.answer.findMany({
+      where: {
+        questionId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return answers.map(PrismaAnswerMapper.toDomain)
+  }
+
+  async create(answer: Answer): Promise<void> {
+    const data = PrismaAnswerMapper.toPrisma(answer)
+
+    await this.prisma.answer.create({
+      data,
+    })
+  }
+
+  async save(answer: Answer): Promise<void> {
+    const data = PrismaAnswerMapper.toPrisma(answer)
+
+    await this.prisma.answer.update({
+      where: {
+        id: answer.id.toString(),
+      },
+      data,
+    })
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    await this.prisma.answer.delete({
+      where: {
+        id: answer.id.toString(),
+      },
+    })
+  }
+}
