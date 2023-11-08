89714be490065ba308697e5d075685c743eb4708
Raul Effting <rauleffting94@gmail.com>
2023-11-08 11:56:54 -0300

    test(controllers): add UploadAttachmentController tests

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { DatabaseModule } from '@/infra/database/database.module'
+import { AppModule } from '../../app.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
+
+describe('Upload attachment (E2E)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    studentFactory = moduleRef.get(StudentFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post('/attachments')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/sample-e2e.jpeg')
+
+    expect(response.statusCode).toBe(201)
+  })
+})
